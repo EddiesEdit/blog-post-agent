9601ea1db497042c86952fd40dfebb991f9468e5
Eddie <endureifi@gmail.com>
2026-01-06 14:14:28 -0800

    creating the agent and and the runner in the main.py

--- agent.py
+++ agent.py
@@ -1,8 +1,61 @@
-from google.adk.agents.llm_agent import Agent
+from google.adk.agents import Agent, SequentialAgent
+from google.adk.model.google_llm import Gemini
+from google.genai import types
+from google.adk.tools import google_search
 
-root_agent = Agent(
-    model='gemini-2.5-flash-lite',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+retry_config = types.HttpRetyOptions(
+    attempts = 5,
+    exp_base= 7,
+    initial_delay = 1,
+    http_status_codes = [429, 500, 503, 504 ],
+)
+
+
+"""
+Blog Post Creation with Sequential Agents
+Let's build a system with three specialized agents:
+
+Outline Agent - Creates a blog outline for a given topic
+Writer Agent - Writes a blog post
+Editor Agent - Edits a blog post draft for clarity and structure
+
+"""
+
+outline_agent = Agent(
+    name ="Outline Agent",
+    model =Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_config=retry_config
+    ),
+    instuction = """Create a blog outline for the given topic with:
+    1. A catchy headline
+    2. An introduction hook
+    3. 3-5 main sections with 2-3 bullet points for each
+    4. A concluding thought""",
+    output_key ="blog_outline"
+)
+writer_agent = Agent(
+    name = "Writer Agent",
+    model = Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_config=retry_config,
+    ),
+    instruction = """Following this outline strictly: {blog_outline}
+    Write a brief, 200 to 300-word blog post with an engaging and informative tone.""",
+    output_key = "blog_draft"
+)
+editor_agent = Agent(
+    name ="Editor Agent",
+    model = Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_config= retry_config,
+    ),
+    instruction ="""Edit this draft: {blog_draft}
+    Your task is to polish the text by fixing any grammatical errors, improving the flow and sentence structure, and enhancing overall clarity.""",
+    output_key ="final_blog_post"
+)
+
+blog_post_agent = SequentialAgent(
+    name ="Blog Pipeline",
+    sub_agents =[outline_agent, writer_agent, editor_agent]
 )
---
+++ main.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import asyncio
+from google.adk.runners import InMemoryRunner
+from agent import root_agent
+from rich.console import Console
+from rich.markdown import Markdown
+console = Console()
+
+
+def extract_final_text(response):
+    """
+    Extract only the final readable text from ADK events.
+    """
+    final_text = None
+
+    for event in response:
+        if not event.content:
+            continue
+
+        for part in event.content.parts:
+            # Only keep normal text (ignore function calls / metadata)
+            if getattr(part, "text", None):
+                final_text = part.text  # keep overwriting until last
+
+    return final_text
+
+
+async def main():
+    runner = InMemoryRunner(agent=root_agent)
+
+    response = await runner.run_debug(
+        "What are the latest advancements in quantum computing and what do they mean for AI?"
+    )
+
+    final_answer = extract_final_text(response)
+
+    #print("\n=== FINAL ANSWER ===\n")
+    console.print(Markdown(final_answer))
+    #print(final_answer)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
